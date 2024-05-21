d121c50f1b8de0ea6445de9a92a62898e6cbcebf
FlankerSK <communicationGCY@outlook.com>
2024-05-21 10:41:23 +0800

    创建项目

---
+++ src/main/java/com/practicum/neuron/NeuronApplication.java
@@ -0,0 +1,13 @@
+package com.practicum.neuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuronApplication.class, args);
+	}
+
+}
